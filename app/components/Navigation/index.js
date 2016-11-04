88046cdd2c2d7f59330729007ffa2d037ded29b7
Geert Huls <geert.huls82@gmail.com>
2016-11-04 23:01:28 +0100

    Pass down props to navigation container and component using redux store

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -9,12 +9,21 @@ import React from 'react';
 
 import styles from './styles.css';
 
-function Navigation() {
+function Navigation({ topics }) {
   return (
     <div className={styles.navigation}>
-      This is the navigation component.
+      We have {topics.length} topics in the nav component.
     </div>
   );
 }
+
+Navigation.propTypes = {
+  topics: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      description: React.PropTypes.string.isRequired,
+    })
+  ),
+};
 
 export default Navigation;
