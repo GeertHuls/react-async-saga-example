f5ae5d64c43915af9e2da0ab1f1c08c94b7249e1
Geert Huls <geert.huls82@gmail.com>
2016-11-13 21:42:09 +0100

    Handle response from server in saga

--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,18 +1,23 @@
 // import { take, call, put, select } from 'redux-saga/effects';
 import { REQUEST_TOPICS } from './constants';
 import { takeLatest } from 'redux-saga';
-import { call } from 'redux-saga/effects';
+import { call, put } from 'redux-saga/effects';
+import { requestTopicsSucceeded, requestTopicsFailed } from './actions';
 
 export function fetchTopicsFromServer() {
-  return fetch('http://locahost:3000/api/topics')
+  return fetch('http://localhost:3000/api/topics')
     .then(response => response.json());
 }
 
 // Generator functions: do async logic without
 // using callbacks.
 function* fetchTopics() {
-  const topics = yield call(fetchTopicsFromServer);
-  console.log('Topics from server: ', topics);
+  try {
+    const topics = yield call(fetchTopicsFromServer);
+    yield put(requestTopicsSucceeded(topics));
+  } catch (e) {
+    yield put(requestTopicsFailed(e.message));
+  }
 }
 
 export function* fetchTopicsSaga() {
