c45a9f3f1cb04bf42aa4306cfbc3934534d8627e
Geert Huls <geert.huls82@gmail.com>
2016-11-13 21:34:27 +0100

    Setup sage to fetch topics from server async

--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,11 +1,25 @@
 // import { take, call, put, select } from 'redux-saga/effects';
+import { REQUEST_TOPICS } from './constants';
+import { takeLatest } from 'redux-saga';
+import { call } from 'redux-saga/effects';
 
-// Individual exports for testing
-export function* defaultSaga() {
-  return;
+export function fetchTopicsFromServer() {
+  return fetch('http://locahost:3000/api/topics')
+    .then(response => response.json());
+}
+
+// Generator functions: do async logic without
+// using callbacks.
+function* fetchTopics() {
+  const topics = yield call(fetchTopicsFromServer);
+  console.log('Topics from server: ', topics);
+}
+
+export function* fetchTopicsSaga() {
+  yield* takeLatest(REQUEST_TOPICS, fetchTopics);
 }
 
 // All sagas to be loaded
 export default [
-  defaultSaga,
+  fetchTopicsSaga,
 ];
