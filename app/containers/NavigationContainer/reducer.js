71385409a03b0dea21f51093fb5cc88a6d5d119a
Geert Huls <geert.huls82@gmail.com>
2016-11-13 21:45:13 +0100

    Finally set topics received from saga on the view's state

--- app/containers/NavigationContainer/reducer.js
+++ app/containers/NavigationContainer/reducer.js
@@ -5,26 +5,19 @@
  */
 
 import { fromJS } from 'immutable';
+import {
+  REQUEST_TOPICS_SUCCEEDED,
+} from './constants';
 
 const initialState = fromJS({
-  topics: [
-    {
-      name: 'libraries',
-      description: 'links to useful open source libraries',
-    },
-    {
-      name: 'apps',
-      description: 'links to new apps',
-    },
-    {
-      name: 'news',
-      description: 'links to programming related news articles',
-    },
-  ],
+  topics: [],
 });
 
 function navigationContainerReducer(state = initialState, action) {
   switch (action.type) {
+    case REQUEST_TOPICS_SUCCEEDED:
+      return state.set('topics', action.topics);
+
     default:
       return state;
   }
