b51a9daba2d1f254fbf06478cdfe6c0cb66b9577
Geert Huls <geert.huls82@gmail.com>
2016-11-18 21:18:42 +0100

    Show topics in divs

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -10,9 +10,15 @@ import React from 'react';
 import styles from './styles.css';
 
 function Navigation({ topics }) {
+  const topicNodes = topics.map(t => (
+    <div key={t.name} >
+      {t.name}
+    </div>
+  ));
+
   return (
     <div className={styles.navigation}>
-      We have {topics.length} topics in the nav component.
+      {topicNodes}
     </div>
   );
 }
