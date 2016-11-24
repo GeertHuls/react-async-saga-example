69b8bc9ec9dc17ae191e47d9e1c318baf53ca84d
Geert Huls <geert.huls82@gmail.com>
2016-11-24 21:45:10 +0100

    Scafold new LinkList component

---
+++ app/components/LinkList/index.js
@@ -0,0 +1,19 @@
+/**
+*
+* LinkList
+*
+*/
+
+import React from 'react';
+
+
+import styles from './styles.css';
+
+function LinkList() {
+  return (
+    <div className={styles.linkList}>
+    </div>
+  );
+}
+
+export default LinkList;
