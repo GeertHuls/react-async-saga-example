1e962679702123ed0437a5430417c19eed7f11a2
Geert Huls <geert.huls82@gmail.com>
2016-11-24 22:13:41 +0100

    Setup link list component with hard coded values in store

--- app/containers/LinkListContainer/reducer.js
+++ app/containers/LinkListContainer/reducer.js
@@ -6,15 +6,22 @@
 
 import { fromJS } from 'immutable';
 import {
-  DEFAULT_ACTION,
+
 } from './constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  links: [
+    {
+      description: 'This is the link description',
+      url: 'https://github.com/facebook/react',
+      topicName: 'links',
+      id: 'fake',
+    },
+  ],
+});
 
 function linkListContainerReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
     default:
       return state;
   }
