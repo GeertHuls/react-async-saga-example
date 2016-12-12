add2a6292b30bfca29302b86ab465246cd45907a
Geert Huls <geert.huls82@gmail.com>
2016-12-12 22:31:49 +0100

    Load links from server instead

--- app/containers/LinkListContainer/reducer.js
+++ app/containers/LinkListContainer/reducer.js
@@ -6,22 +6,17 @@
 
 import { fromJS } from 'immutable';
 import {
-
+  REQUEST_LINKS_SUCCEEDED,
 } from './constants';
 
 const initialState = fromJS({
-  links: [
-    {
-      description: 'This is the link description',
-      url: 'https://github.com/facebook/react',
-      topicName: 'links',
-      id: 'fake',
-    },
-  ],
+  links: [],
 });
 
 function linkListContainerReducer(state = initialState, action) {
   switch (action.type) {
+    case REQUEST_LINKS_SUCCEEDED:
+      return state.set('links', action.links);
     default:
       return state;
   }
