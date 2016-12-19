4f1ffb61def75b20ba45c54ca274a0d1e4672c5c
Geert Huls <geert.huls82@gmail.com>
2016-12-19 23:32:12 +0100

    Close drawer after topic selection

--- app/containers/NavigationContainer/reducer.js
+++ app/containers/NavigationContainer/reducer.js
@@ -22,7 +22,8 @@ function navigationContainerReducer(state = initialState, action) {
       return state.set('topics', action.topics);
 
     case SELECT_TOPIC:
-      return state.set('selectedTopic', action.topic);
+      return state.set('selectedTopic', action.topic)
+        .set('isDrawerOpen', false);
 
     case TOGGLE_DRAWER:
       return state.set('isDrawerOpen', !state.get('isDrawerOpen'));
