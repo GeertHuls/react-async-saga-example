bf53cfc43df16e21238bd76a0500b923b2178e94
Geert Huls <geert.huls82@gmail.com>
2016-12-19 22:39:46 +0100

    App bar styling and content

--- app/components/AppBar/index.js
+++ app/components/AppBar/index.js
@@ -5,14 +5,31 @@
 */
 
 import React from 'react';
-
+import FontAwesome from 'react-fontawesome';
 
 import styles from './styles.css';
 
 function AppBar() {
   return (
     <div className={styles.appBar}>
-      Coder daily
+      <div
+        className={styles.iconButton}
+      >
+        <FontAwesome
+          className={styles.icon}
+          name="bars"
+        />
+      </div>
+      <div
+        className={styles.heading}
+      >
+        Coder daily
+      </div>
+      <div
+        className={styles.linkContainer}
+      >
+        log in
+      </div>
     </div>
   );
 }
