a7788b468a2e472832151314c10081ee0d2df5f4
Geert Huls <geert.huls82@gmail.com>
2016-12-19 23:55:33 +0100

    Link component styling

--- app/components/Link/index.js
+++ app/components/Link/index.js
@@ -12,7 +12,32 @@ import styles from './styles.css';
 function Link({ link }) {
   return (
     <div className={styles.link}>
-      {link.url}
+      <div
+        className={styles.votingContainer}
+      >
+        <div
+          className={styles.votingCount}
+        >
+          {link.voteCount}
+        </div>
+      </div>
+      <div
+        className={styles.detailsContainer}
+      >
+        <div>
+          <a
+            href={link.url}
+            className={styles.linkAnchor}
+          >
+            {link.url}
+          </a>
+        </div>
+        <div
+          className={styles.description}
+        >
+          {link.description}
+        </div>
+      </div>
     </div>
   );
 }
