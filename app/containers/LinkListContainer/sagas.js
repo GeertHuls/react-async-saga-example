8b75de2ce86def515b24f8ed8d14591dc909289b
Geert Huls <geert.huls82@gmail.com>
2016-12-21 12:15:43 +0100

    Moved select topic event handler to navigation container

--- app/containers/LinkListContainer/sagas.js
+++ app/containers/LinkListContainer/sagas.js
@@ -1,7 +1,6 @@
 // import { take, call, put, select } from 'redux-saga/effects';
 import { call, put } from 'redux-saga/effects';
 import { takeLatest } from 'redux-saga';
-import { SELECT_TOPIC } from '../NavigationContainer/constants';
 import { requestLinksSucceeded, requestLinksFailed } from './actions';
 
 function fetchLinksFromServer(topic) {
@@ -19,15 +18,6 @@ function* fetchLinks(action) {
   }
 }
 
-// Individual exports for testing
-export function* defaultSaga() {
-  // takeLatest listens on the SELECT_TOPIC event
-  // waiting to get fired. When it does, it'll
-  // execute the fetchLinks function.
-  yield* takeLatest(SELECT_TOPIC, fetchLinks);
-}
-
 // All sagas to be loaded
 export default [
-  defaultSaga,
 ];
--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,5 +1,5 @@
 // import { take, call, put, select } from 'redux-saga/effects';
-import { REQUEST_TOPICS } from './constants';
+import { REQUEST_TOPICS, SELECT_TOPIC } from './constants';
 import { takeLatest } from 'redux-saga';
 import { call, put } from 'redux-saga/effects';
 import { requestTopicsSucceeded, requestTopicsFailed } from './actions';
@@ -20,6 +20,19 @@ function* fetchTopics() {
   }
 }
 
+function* pushTopic(action) {
+  // in here we will write logic to change the url
+  // for the relevant topic.
+  console.log(action);
+}
+
+export function* pushTopicSaga() {
+  // takeLatest listens on the SELECT_TOPIC event
+  // waiting to get fired. When it does, it'll
+  // execute the fetchLinks function.
+  yield* takeLatest(SELECT_TOPIC, pushTopic);
+}
+
 export function* fetchTopicsSaga() {
   yield* takeLatest(REQUEST_TOPICS, fetchTopics);
 }
@@ -27,4 +40,5 @@ export function* fetchTopicsSaga() {
 // All sagas to be loaded
 export default [
   fetchTopicsSaga,
+  pushTopicSaga,
 ];
