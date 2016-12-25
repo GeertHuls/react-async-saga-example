08746318f817abd072005a3c6e83d8d10fc719d0
Geert Huls <geert.huls82@gmail.com>
2016-12-25 22:39:54 +0100

    Created overlay style for link form component

--- app/components/LinkForm/index.js
+++ app/components/LinkForm/index.js
@@ -14,40 +14,42 @@ class LinkForm extends React.Component { // eslint-disable-line react/prefer-sta
 
   render() {
     return (
-      <div className={styles.linkForm}>
-        <div
-          className={styles.heading}
-        >
-          Add a link
-        </div>
-
-        <input
-          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
-          placeholder="URL"
-          ref={(f) => { this.urlField = f; }}
-          type="text"
-        />
-
-        <input
-          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
-          placeholder="Description"
-          ref={(f) => { this.descriptionField = f; }}
-          type="text"
-        />
-
-        <div
-          className={styles.actionContainer}
-        >
+      <div className={styles.overlay}>
+        <div className={styles.linkForm}>
           <div
-            className={styles.button}
+            className={styles.heading}
           >
-            cancel
+            Add a link
           </div>
+
+          <input
+            className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
+            placeholder="URL"
+            ref={(f) => { this.urlField = f; }}
+            type="text"
+          />
+
+          <input
+            className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
+            placeholder="Description"
+            ref={(f) => { this.descriptionField = f; }}
+            type="text"
+          />
+
           <div
-            className={styles.button}
-            onClick={this.login}
+            className={styles.actionContainer}
           >
-            log in
+            <div
+              className={styles.button}
+            >
+              cancel
+            </div>
+            <div
+              className={styles.button}
+              onClick={this.login}
+            >
+              log in
+            </div>
           </div>
         </div>
       </div>
