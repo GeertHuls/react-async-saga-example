92a4f05859e5e78f96f754ec29ebc4dba7e033a8
Geert Huls <geert.huls82@gmail.com>
2016-12-25 23:30:12 +0100

    Re-use text input component for login

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -8,7 +8,7 @@ import React from 'react';
 
 import styles from './styles.css';
 import validator from 'email-validator';
-import classNames from 'classnames';
+import TextInput from '../TextInput';
 
 class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
@@ -20,7 +20,7 @@ class Login extends React.Component { // eslint-disable-line react/prefer-statel
   state = {};
 
   login = () => {
-    const email = this.emailField.value;
+    const email = this.emailField.value();
     if (!validator.validate(email)) {
       this.setState({
         errorText: 'Please provide a valid email',
@@ -36,14 +36,6 @@ class Login extends React.Component { // eslint-disable-line react/prefer-statel
   }
 
   render() {
-    const fieldError = this.state.errorText ? (
-      <div
-        className={styles.errorMessage}
-      >
-        {this.state.errorText}
-      </div>
-    ) : null;
-
     return (
       <div className={styles.login}>
         <div
@@ -52,14 +44,11 @@ class Login extends React.Component { // eslint-disable-line react/prefer-statel
           Login with your email
         </div>
 
-        <input
-          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
+        <TextInput
           placeholder="Your email"
           ref={(f) => { this.emailField = f; }}
-          type="text"
+          errorText={this.state.errorText}
         />
-
-        {fieldError}
 
         <div
           className={styles.actionContainer}
