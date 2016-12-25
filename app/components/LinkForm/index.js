bc64b401f1ab09874f304ba7f822f4a88ab16904
Geert Huls <geert.huls82@gmail.com>
2016-12-25 22:27:42 +0100

    Initial styled link form component

--- app/components/LinkForm/index.js
+++ app/components/LinkForm/index.js
@@ -7,11 +7,49 @@
 import React from 'react';
 
 import styles from './styles.css';
+import classNames from 'classnames';
 
 class LinkForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  state = {};
+
   render() {
     return (
       <div className={styles.linkForm}>
+        <div
+          className={styles.heading}
+        >
+          Add a link
+        </div>
+
+        <input
+          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
+          placeholder="URL"
+          ref={(f) => { this.urlField = f; }}
+          type="text"
+        />
+
+        <input
+          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
+          placeholder="Description"
+          ref={(f) => { this.descriptionField = f; }}
+          type="text"
+        />
+
+        <div
+          className={styles.actionContainer}
+        >
+          <div
+            className={styles.button}
+          >
+            cancel
+          </div>
+          <div
+            className={styles.button}
+            onClick={this.login}
+          >
+            log in
+          </div>
+        </div>
       </div>
     );
   }
