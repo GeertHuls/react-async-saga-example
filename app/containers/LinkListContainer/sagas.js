6a0b5c5df6099f7eed65a77f1dedbd0f89792e3a
Geert Huls <geert.huls82@gmail.com>
2016-12-27 22:29:48 +0100

    Add button handler

--- app/containers/LinkListContainer/sagas.js
+++ app/containers/LinkListContainer/sagas.js
@@ -2,7 +2,8 @@
 import { call, put } from 'redux-saga/effects';
 import { takeLatest } from 'redux-saga';
 import { requestLinksSucceeded, requestLinksFailed } from './actions';
-import { REQUEST_LINKS } from './constants';
+import { REQUEST_LINKS, START_ADD } from './constants';
+import { push } from 'react-router-redux';
 
 function fetchLinksFromServer(topicName) {
   return fetch(`http://localhost:3000/api/topics/${topicName}/links`)
@@ -23,7 +24,16 @@ export function* defaultSaga() {
   yield* takeLatest(REQUEST_LINKS, fetchLinks);
 }
 
+function* startAdd(action) {
+  yield put(push(`/topics/${action.topicName}/add`));
+}
+
+export function* startAddSaga() {
+  yield takeLatest(START_ADD, startAdd);
+}
+
 // All sagas to be loaded
 export default [
   defaultSaga,
+  startAddSaga,
 ];
